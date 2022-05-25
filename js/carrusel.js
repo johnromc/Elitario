56ee1eed930c7987d585a49f7dfedbe3ae5d667b
John Romero <j.s.a.jr23@gmail.com>
2022-05-25 12:07:15 -0500

    finalización de las paginas index, about y la primera página del blog, queda haciendo falta agregar la interactividad con JS

--- js/carrusel.js
+++ js/carrusel.js
@@ -1,9 +1,25 @@
-const $nextBtn = document.querySelector('.next-btn'),
+const 
+$nextBtn = document.querySelector('.next-btn'),
 $prevBtn = document.querySelector('.prev-btn'),
 $slides  = document.querySelectorAll('.slide'),
 $slidesIcon= document.querySelectorAll('.slide-icon'),
 numberOfSlides = $slides.length;
 let slideNumber = 0;
+
+const 
+$cardSale = document.querySelector('.card-sale'),
+$button = document.querySelectorAll('.add-card');
+
+const 
+$panel = document.querySelector('.panel'),
+$panelBtn = document.querySelector('.hamburger');
+
+
+$panelBtn.addEventListener('click',(e)=>{
+    $panel.classList.toggle('active')
+    $panelBtn.classList.toggle('is-active')
+})
+
 
 $nextBtn.addEventListener('click',()=>{
     $slides.forEach((slide) =>{
@@ -23,3 +39,9 @@ $nextBtn.addEventListener('click',()=>{
     $slides[slideNumber].classList.add('active');
     $slidesIcon[slideNumber].classList.add('active');
 })
+
+for(i = 0 ; i < $cardSale.length; i++){
+    $cardSale[i].addEventListener('mouseover',(e)=>{
+        alert('hola')
+    })
+}
--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,2 @@
-const $panel = document.querySelector('.panel'),
-$panelBtn = document.querySelector('.hamburger');
 
-$panelBtn.addEventListener('click',(e)=>{
-    $panel.classList.toggle('active')
-    $panelBtn.classList.toggle('is-active')
-})
+
